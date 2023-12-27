d6556040ca5f17a45e9439a2744cd65e8a43ae17
nikolaizombie1 <fabio.matos999@gmail.com>
2023-12-27 11:22:30 -0500

    Fix struct placement.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,11 +1,12 @@
 use actix_web::{web, HttpResponse};
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
     name: String,
 }
+
+pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
